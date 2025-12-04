3a37271a39b9c95dee7bbaa62e1e599e57585641
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-12-04 13:59:29 +0000

    chore

--- database/seed.py
+++ database/seed.py
@@ -1,9 +1,13 @@
+import peewee as _PW
+import sqlite3 as _SQLITE3
+
 from client import DBClient
+from schema import ENUM__Permission_level, ENUM__Ticket_holderType
 
 dbClient = DBClient()
 
 
-filmTitles = [
+FILM_TITLES = [
   "Jurassic Cabin",
   "The Dark Night",
   "The Nightmare on First Street",
@@ -12,7 +16,28 @@ filmTitles = [
   "The Shape of Time"
 ]
 
-for filmTitle in filmTitles:
+
+
+# === Populate the database with data we know we'll need ===
+# We use `Model.get_or_create()` so we don't get an error if the data already exists
+
+# Admin User
+adminUser = dbClient.tables.User.get_or_create(
+  username="admin",
+  password="123"
+)
+adminPermission = dbClient.tables.Permission.get_or_create(
+  level=ENUM__Permission_level.ADMIN
+)
+dbClient.tables.UserPermissions.get_or_create(
+  user=adminUser,
+  permission=adminPermission
+)
+
+# Films from the Film Catalogue
+for filmTitle in FILM_TITLES:
   dbClient.tables.Film.get_or_create(
     title=filmTitle
   )
+
+# === Populate the database with data we know we'll need ===
