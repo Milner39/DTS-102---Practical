f9e315f7dd83e28939b57614977752837e70fa15
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-12-04 16:01:51 +0000

    chore

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from database import DBClient
+from database import user__dataByAuth
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+userData = user__dataByAuth(username, password)
+
+if (userData == None):
+  print("Incorrect username/password combination")
+else:
+  print("Here's your data")
+  print(userData)
