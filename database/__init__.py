eb623ee639f953cba6878da5a20e0a1d7c3990a7
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-12-04 15:24:50 +0000

    feat: function to get user data if login is correct

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,37 @@
+import peewee as _PW
+
 from client import DBClient
+
+
+dbClient = DBClient()
+
+
+
+def user__idByAuth(username: str, password: str):
+  User = dbClient.tables.User
+
+  try:
+    user = User.get(
+      (User.username == username) &
+      (User.password == password)
+    )
+    return user.id
+  except _PW.DoesNotExist:
+    return None
+
+
+def user__dataByAuth(username: str, password: str):
+  User = dbClient.tables.User
+
+  userId = user__idByAuth(username, password)
+  if userId == None:
+    return None
+  
+  query = (User
+    .select(User.username, User.contactPhone)
+    .where(User.id == userId)
+  )
+  return list(query.dicts())
+
+
+print(user__dataByAuth("admin", "123"))
