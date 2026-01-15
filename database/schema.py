2ab37c3f01e667fc297ff6875c5ff4b5b5ffc934
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-01-15 19:42:21 +0000

    refactor: needs fixes

--- database/schema.py
+++ database/schema.py
@@ -12,6 +12,9 @@ This file defines the schema for our tables, and the relationships between them.
 A schema tell our database client what tables should exist in our database, and 
 the datatypes of the fields in the tables.
 
+The schema is made up from `Model` objects, each defines the structure of a 
+table in the database.
+
 When defining the tables, we can add constraints on each field like 
 `primary_key` for primary keys, `unique` to enforce unique values, and `null` 
 to prevent `None` values.
@@ -21,302 +24,325 @@ We also can do more complicated things like composite keys and indexes.
 
 
 
-"""
-We define this database proxy (fake database object) so the base table model 
-has a reference to the database. We "swap in" the real database later.
+class Schema:
+  """The Schema module"""
 
-This is just a requirement when using `peewee`, I don't fully understand it, 
-but if you do, maybe improve this comment.
-"""
-_dbProxy = _PW.DatabaseProxy()
-
-
-
-# === Define a class to extend when creating the schema for db tables ===
-
-class BaseModel(_PW.Model):
-  # This lets us give all the tables some common attributes
-  class Meta:
-    # This tells each table class what database it's in
-    database = _dbProxy
-    legacy_table_names = False
-
-
-# `peewee` removes Meta at runtime, keep a stand-in for type checkers to ignore 
-# annoying syntax messages.
-if _TYPING.TYPE_CHECKING:
-  BaseMeta = BaseModel.Meta
-else:
-  class BaseMeta:
-    pass
-
-# === ===
-
-
-
-# === Define some reusable field types ===
-
-# Saves us re-writing common field types
-
-pk_uuid = lambda : _PW.UUIDField(
-  primary_key=True,
-  default=_UUID.uuid4,
-  unique=True,
-  null=False,
-)
-"""Primary Key that generates a random UUID on creation"""
-
-# === ===
-
-
-
-# === Define some enums for enum fields ===
-
-# These are for tables where we know there are a finite amount of entries, 
-# and we know the entries before hand.
-
-class PermissionGroup_ENUM(_ENUM.Enum):
-  """Enum for the `PermissionGroup` table"""
-  ADMIN = 0
-  DEVELOPER = 1
-
-class TicketHolderType_ENUM(_ENUM.Enum):
-  """Enum for the `TicketHolderType` table"""
-  ADULT = 0
-  TEENAGER = 1
-  CHILD = 2
-  STUDENT = 3
-
-# === ===
-
-
-#region Table Schemas
-# === Define the table schemas ===
-
-class User(BaseModel):
-  """Users of this application"""
-
-  id = pk_uuid()
-  """UUID primary key"""
-
-  username = _PW.CharField(
-    max_length=64,
-    unique=True,
-    null=False,
-  )
-  """A unique, non-null username used for registration / login"""
-
-  password = _PW.CharField(
-    max_length=64,
-    null=False,
-  )
   """
-  Used to authenticate a user by their username
-  
-  Will be stored as plain-text because encryption is out of scope
+  We define this database proxy (fake database object) so the base table model 
+  has a reference to the database. We "swap in" the real database later.
+
+  This is just a requirement when using `peewee`, I don't fully understand it, 
+  but if you do, maybe you can improve this comment.
   """
-
-  contactPhone = _PW.CharField(
-    max_length=32,
-    null=True,
-  )
-  """
-  An optional phone number (string) used to contact the user
-  """
+  __dbProxy = _PW.DatabaseProxy()
 
 
 
-class PermissionGroup(BaseModel):
-  """Groups that determine what permissions a user has"""
+  #region ENUMs
 
-  id = _PW.SmallIntegerField(
-    primary_key=True,
-    unique=True,
-    null=False,
-  )
-  """The value of the member in `PermissionGroup_ENUM`"""
+  class ENUMs:
+    """A class containing all of the `ENUM` classes for populating tables"""
 
-  readable = _PW.CharField(
-    max_length=32,
-    unique=True,
-    null=False,
-  )
-  """The name of the member in `PermissionGroup_ENUM`"""
+    # These are for tables where we know there are a finite amount of entries, 
+    # and we know the entries before hand.
 
+    class PermissionGroup_ENUM(_ENUM.Enum):
+      """Enum for the `PermissionGroup` table"""
+      ADMIN = 0
+      DEVELOPER = 1
 
+    class TicketHolderType_ENUM(_ENUM.Enum):
+      """Enum for the `TicketHolderType` table"""
+      ADULT = 0
+      TEENAGER = 1
+      CHILD = 2
+      STUDENT = 3
 
-class UserPermissionGroups(BaseModel):
-  """Join table for `Users` and `PermissionGroup`"""
-
-  user = _PW.ForeignKeyField(User,
-    backref="permissionGroups",
-    on_update="CASCADE",
-    on_delete="CASCADE",
-  )
-  """Entry in `User` table"""
-
-  permissionGroup = _PW.ForeignKeyField(PermissionGroup,
-    backref="users",
-    on_update="CASCADE",
-    on_delete="CASCADE",
-  )
-  """Entry in `PermissionGroup` table"""
-
-  class Meta(BaseMeta):
-    primary_key = _PW.CompositeKey("user", "permissionGroup")
+  #endregion ENUMs
 
 
 
-class Film(BaseModel):
-  """All past and future films that have and will be shown"""
+  #region Fields
 
-  title = _PW.CharField(
-    primary_key=True,
-    max_length=64,
-    unique=True,
-    null=False,
-  )
-  """The title of the film"""
+  class Fields:
+    """A class containing all of the custom `Field` objects"""
+
+    # === Define a base class to extend when creating Fields ===
+
+    class BaseField:
+      """
+      A base Fields to extend when creating custom Fields for the Models.
+
+      This lets us give all the Fields some common attributes.
+      """
+
+      @staticmethod
+      def create() -> _PW.Field:
+        return _PW.Field()
+
+    # === ===
 
 
-class Booking(BaseModel):
-  """Bookings for films made by users"""
+    class PkUUID(BaseField):
+      """Primary Key that generates a random UUID on creation"""
 
-  id = pk_uuid()
-  """UUID primary key"""
+      @staticmethod
+      def create():
+        return _PW.UUIDField(
+          primary_key=True,
+          default=_UUID.uuid4,
+          unique=True,
+          null=False,
+        )
 
-  user = _PW.ForeignKeyField(User,
-    backref="bookings",
-    on_update="CASCADE"
-    # Do not delete bookings if the user has been deleted
-  )
-  """Entry in `User` table"""
-
-  film = _PW.ForeignKeyField(Film,
-    backref="bookings",
-    on_update="CASCADE",
-    on_delete="CASCADE",
-  )
-  """Entry in `Film` table"""
-
-  datetime = _PW.DateTimeField(
-    null=False
-  )
-  """
-  Date & time of the showing of the film
-
-  We assume that all films are always showing so a user can make a booking for 
-  any film at any time, because this is just a College assignment, and not the 
-  real world :)
-  """
+  #endregion Fields
 
 
 
-class TicketHolderType(BaseModel):
-  """Ticket types that determine how much a ticket costs"""
+  #region Models
 
-  id = _PW.SmallIntegerField(
-    primary_key=True,
-    unique=True,
-    null=False,
-  )
-  """The value of the member in `TicketHolderType_ENUM`"""
+  class Models:
+    """A class containing all of the `Model` objects (tables)"""
 
-  readable = _PW.CharField(
-    max_length=32,
-    unique=True,
-    null=False,
-  )
-  """The name of the member in `TicketHolderType_ENUM`"""
+    # === Define a base class to extend when creating Models ===
 
+    class BaseModel(_PW.Model):
+      """
+      A base Model to extend when creating Models for the database schema.
 
+      This lets us give all the Models some common attributes.
+      """
 
-class Ticket(BaseModel):
-  """Tickets for bookings made by users"""
-
-  id = pk_uuid()
-  """UUID primary key"""
-
-  booking = _PW.ForeignKeyField(Booking,
-    backref="tickets",
-    on_update="CASCADE",
-    on_delete="CASCADE",
-  )
-  """Entry in `Booking` table"""
-
-  holderName = _PW.CharField(
-    max_length=64
-  )
-  """Optional ticket holder name"""
-
-  holderType = _PW.ForeignKeyField(TicketHolderType,
-    backref="tickets",
-  )
-  """Entry in `TicketHolderType` table"""
-
-  paidPriceGBP = _PW.DecimalField(
-    max_digits=5,
-    decimal_places=2,
-    auto_round=True,
-    null=False
-  )
-  """
-  The calculated amount the user paid for the ticket at the point of booking.
-
-  This *would* let us refund the correct amount to the user in case of a 
-  cancellation, and makes sure the refunded amount isn't too little / much 
-  because of potential price changes / deals.
-  """
-
-# === ===
-#endregion Table Schemas
+      class Meta:
+        # This tells each table class what database it's in
+        database = Schema.__dbProxy
+        legacy_table_names = False
 
 
+    """
+    `peewee` removes `Model.Meta` at runtime, keep a stand-in for type checkers 
+    to ignore annoying syntax messages.
+    """
+    if _TYPING.TYPE_CHECKING:
+      BaseMeta = BaseModel.Meta
+    else:
+      class BaseMeta:
+        pass
 
-#region Utils
-# === Define some utility classes / subroutines to help use the schema
-
-class Tables:
-  User = User
-  UserPermissionGroups = UserPermissionGroups
-  PermissionGroup = PermissionGroup
-  Film = Film
-  Booking = Booking
-  Ticket = Ticket
-  TicketHolderType = TicketHolderType
-
+    # === ===
 
 
-def initializeProxy(database: _PW.Database):
-  """"
-  Swaps in" the real database object to the proxy.
+    class User(BaseModel):
+      """Users of this application"""
 
-  This must be done before creating the tables.
-  """
-  _dbProxy.initialize(database)
+      id = Fields.PkUUID.create()
+      """UUID primary key"""
+
+      username = _PW.CharField(
+        max_length=64,
+        unique=True,
+        null=False,
+      )
+      """A unique, non-null username used for registration / login"""
+
+      password = _PW.CharField(
+        max_length=64,
+        null=False,
+      )
+      """
+      Used to authenticate a user by their username
+      
+      Will be stored as plain-text because encryption is out of scope
+      """
+
+      contactPhone = _PW.CharField(
+        max_length=32,
+        null=True,
+      )
+      """
+      An optional phone number (string) used to contact the user
+      """
 
 
-def createTables(database: _PW.Database):
-  """
-  Initialise the database proxy and create all tables defined in the schema.
+    class PermissionGroup(BaseModel):
+      """Groups that determine what permissions a user has"""
 
-  Returns all the tables.
-  """
+      id = _PW.SmallIntegerField(
+        primary_key=True,
+        unique=True,
+        null=False,
+      )
+      """The value of the member in `PermissionGroup_ENUM`"""
 
-  initializeProxy(database)
+      readable = _PW.CharField(
+        max_length=32,
+        unique=True,
+        null=False,
+      )
+      """The name of the member in `PermissionGroup_ENUM`"""
 
-  dbInstance = database
-  dbInstance.create_tables([
-    Tables.User,
-    Tables.UserPermissionGroups,
-    Tables.PermissionGroup,
-    Tables.Film,
-    Tables.Booking,
-    Tables.Ticket,
-    Tables.TicketHolderType
-  ])
 
-  return Tables
+    class UserPermissionGroups(BaseModel):
+      """Join table for `Users` and `PermissionGroup`"""
 
-# === ===
-#endregion Utils
+      user = _PW.ForeignKeyField(Models.User,
+        backref="permissionGroups",
+        on_update="CASCADE",
+        on_delete="CASCADE",
+      )
+      """Entry in `User` table"""
+
+      permissionGroup = _PW.ForeignKeyField(Models.PermissionGroup,
+        backref="users",
+        on_update="CASCADE",
+        on_delete="CASCADE",
+      )
+      """Entry in `PermissionGroup` table"""
+
+      class Meta(Models.BaseMeta):
+        primary_key = _PW.CompositeKey("user", "permissionGroup")
+
+
+
+    class Film(BaseModel):
+      """All past and future films that have and will be shown"""
+
+      title = _PW.CharField(
+        primary_key=True,
+        max_length=64,
+        unique=True,
+        null=False,
+      )
+      """The title of the film"""
+
+
+    class Booking(BaseModel):
+      """Bookings for films made by users"""
+
+      id = Fields.PkUUID.create()
+      """UUID primary key"""
+
+      user = _PW.ForeignKeyField(User,
+        backref="bookings",
+        on_update="CASCADE"
+        # Do not delete bookings if the user has been deleted
+      )
+      """Entry in `User` table"""
+
+      film = _PW.ForeignKeyField(Film,
+        backref="bookings",
+        on_update="CASCADE",
+        on_delete="CASCADE",
+      )
+      """Entry in `Film` table"""
+
+      datetime = _PW.DateTimeField(
+        null=False
+      )
+      """
+      Date & time of the showing of the film
+
+      We assume that all films are always showing so a user can make a booking for 
+      any film at any time, because this is just a College assignment, and not the 
+      real world :)
+      """
+
+
+
+    class TicketHolderType(BaseModel):
+      """Ticket types that determine how much a ticket costs"""
+
+      id = _PW.SmallIntegerField(
+        primary_key=True,
+        unique=True,
+        null=False,
+      )
+      """The value of the member in `TicketHolderType_ENUM`"""
+
+      readable = _PW.CharField(
+        max_length=32,
+        unique=True,
+        null=False,
+      )
+      """The name of the member in `TicketHolderType_ENUM`"""
+
+
+
+    class Ticket(BaseModel):
+      """Tickets for bookings made by users"""
+
+      id = Fields.PkUUID.create()
+      """UUID primary key"""
+
+      booking = _PW.ForeignKeyField(Model.Booking,
+        backref="tickets",
+        on_update="CASCADE",
+        on_delete="CASCADE",
+      )
+      """Entry in `Booking` table"""
+
+      holderName = _PW.CharField(
+        max_length=64
+      )
+      """Optional ticket holder name"""
+
+      holderType = _PW.ForeignKeyField(Model.TicketHolderType,
+        backref="tickets",
+      )
+      """Entry in `TicketHolderType` table"""
+
+      paidPriceGBP = _PW.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        auto_round=True,
+        null=False
+      )
+      """
+      The calculated amount the user paid for the ticket at the point of booking.
+
+      This *would* let us refund the correct amount to the user in case of a 
+      cancellation, and makes sure the refunded amount isn't too little / much 
+      because of potential price changes / deals.
+      """
+
+  #endregion Models
+
+
+
+  #region Utils
+
+  class Utils:
+
+    @staticmethod
+    def initializeProxy(database: _PW.Database):
+      """
+      "Swaps in" the real database object to the proxy.
+
+      This must be done before creating the tables.
+      """
+      _dbProxy.initialize(database)
+
+
+    @staticmethod
+    def createTables(database: _PW.Database):
+      """
+      Initialise the database proxy and create all tables defined in the schema.
+
+      Returns all the tables.
+      """
+
+      __class__.initializeProxy(database)
+
+      database.create_tables([
+        Models.User,
+        Models.UserPermissionGroups,
+        Models.PermissionGroup,
+        Models.Film,
+        Models.Booking,
+        Models.Ticket,
+        Models.TicketHolderType
+      ])
+
+      return Models
+
+  #endregion Utils
